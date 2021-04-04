6f2e74caff6c574f1c5afe616e0d04809cb18d96
André Luiz Dias de Oliveira <51346021+andr30z@users.noreply.github.com>
2021-04-03 22:16:55 -0300

    Creating api mockup

---
+++ src/Pages/ActivitySelection/ApiMockup.constant.ts
@@ -0,0 +1,83 @@
+export const Mockup = [
+  {
+    level: 1,
+    name: "Alfabeto",
+    description: "Vamos aprender sobre as letras do alfabeto?",
+    activities: [
+      {
+        name: "Atividade 1",
+        level: 1,
+      },
+      {
+        name: "Atividade 2",
+        level: 2,
+      },
+      {
+        name: "Atividade 3",
+        level: 3,
+      },
+      {
+        name: "Atividade 4",
+        level: 3,
+      },
+      {
+        name: "Atividade 5",
+        level: 4,
+      },
+    ],
+  },
+  {
+    level: 2,
+    name: "Fonemas",
+    description: "Vamos aprender sobre os fonemas?",
+    activities: [
+      {
+        name: "Atividade 1",
+        level: 1,
+      },
+      {
+        name: "Atividade 2",
+        level: 2,
+      },
+      {
+        name: "Atividade 3",
+        level: 2,
+      },
+      {
+        name: "Atividade 4",
+        level: 4,
+      },
+    ],
+  },
+  {
+    level: 3,
+    name: "Silábico",
+    description: "Vamos aprender sobre as silabas?",
+    activities: [
+      {
+        name: "Atividade 1",
+        level: 1,
+      },
+      {
+        name: "Atividade 2",
+        level: 2,
+      },
+      {
+        name: "Atividade 3",
+        level: 3,
+      },
+      {
+        name: "Atividade 4",
+        level: 4,
+      },
+      {
+        name: "Atividade 5",
+        level: 4,
+      },
+      {
+        name: "Atividade 6",
+        level: 4,
+      },
+    ],
+  },
+];
