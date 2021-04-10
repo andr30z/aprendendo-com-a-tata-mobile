089fc45182ca06d189b5be9d2f75e1edf6188b94
André Luiz Dias de Oliveira <51346021+andr30z@users.noreply.github.com>
2021-04-10 13:53:41 -0300

    Creating WithIconContainer HOC

--- src/Components/index.ts
+++ src/Components/index.ts
@@ -1,1 +1,6 @@
 export { default as WithStatusBar } from "./WithStatusBar/WithStatusBar.Component";
+export { default as WithIconContainer } from "./WithIconContainer/WithIconContainer.Component";
+export { default as Input } from "./Input/Input.Component";
+export { default as Badge } from "./Badge/Badge.Component";
+export { default as ActivityItem } from "./ActivityItem/ActivityItem.Component";
+export { default as ActivityGroup } from "./ActivityGroup/ActivityGroup.Component";
