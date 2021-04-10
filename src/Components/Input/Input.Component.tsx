50aed10b931569094a06d72a33ffddcdef011843
André Luiz Dias de Oliveira <51346021+andr30z@users.noreply.github.com>
2021-04-10 01:25:47 -0300

    Apparently if there is an "any" casting on BaseInputProps typescript stops complaining

--- src/Components/Input/Input.Component.tsx
+++ src/Components/Input/Input.Component.tsx
@@ -3,7 +3,7 @@ import { TextInputProps } from "react-native";
 import { BaseInput, BaseInputProps } from "../../GlobalStyles/BaseStyles";
 
 const Input: React.FC<BaseInputProps> = (props) => {
-  return <BaseInput {...props} />;
+  return <BaseInput {...props as any} />;
 };
 
 export default Input;
