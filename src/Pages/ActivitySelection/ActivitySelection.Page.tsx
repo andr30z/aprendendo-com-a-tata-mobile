6cc70359d54d705145a51f426c623ea7509dd197
André Luiz Dias de Oliveira <51346021+andr30z@users.noreply.github.com>
2021-04-10 17:41:03 -0300

    Adding Introduction to activities and scrolling to activities start

--- src/Pages/ActivitySelection/ActivitySelection.Page.tsx
+++ src/Pages/ActivitySelection/ActivitySelection.Page.tsx
@@ -1,25 +1,30 @@
-import React from "react";
-import { BaseText } from "../../GlobalStyles/BaseStyles";
+import React, { useRef, useState } from "react";
+import { Entypo } from "@expo/vector-icons";
 import {
   BaseContainer,
   ScrollContainer,
 } from "../../GlobalStyles/Containers.Style";
 import { Mockup } from "./ApiMockup.constant";
 
-import { StatusBar } from "react-native";
+import { Pressable, StatusBar, View, ScrollView } from "react-native";
 import { CONSTANTS } from "../../Constants";
-import { ActivityList, HeaderActivity } from "./Modules";
+import { ActivityIntroduction, ActivityList, ActivityHeader } from "./Modules";
+import { LinearGradient } from "expo-linear-gradient";
 
 /**
  * ActivitySelectionPage aka Home, thats the place where user is going to select an activity
  * @author andr3z0
  **/
 const ActivitySelection: React.FC = ({ children }) => {
+  const scrollToRef = useRef<ScrollView>();
+  const [scrollPosition, setScrollPosition] = useState<number>(0);
+
   return (
     <ScrollContainer
       style={{ height: "100%" }}
       contentContainerStyle={{ paddingBottom: 100 }}
       backgroundColor="#fff"
+      ref={scrollToRef as any}
     >
       <StatusBar backgroundColor="#8078cc" translucent />
       <BaseContainer
@@ -29,12 +34,50 @@ const ActivitySelection: React.FC = ({ children }) => {
           flex: 1,
           flexDirection: "column",
           backgroundColor: "#8F86E3",
+          position: "relative",
         }}
       >
         {children}
-        <HeaderActivity />
+        <ActivityHeader />
+        <View
+          style={{
+            position: "absolute",
+            bottom: -29,
+            left: 0,
+            right: 0,
+            width: CONSTANTS.DEVICE_WIDTH,
+            alignItems: "center",
+          }}
+        >
+          <Pressable
+            onPress={() => {
+              console.log(scrollPosition, "possss");
+              scrollToRef.current?.scrollTo({
+                y: scrollPosition + 200,
+                x: scrollPosition,
+                animated: true,
+              });
+            }}
+          >
+            <LinearGradient
+              colors={["#8078cc", "#8F86E3"]}
+              style={{
+                borderRadius: 60 / 2,
+                height: 60,
+                width: 60,
+                alignItems: "center",
+                justifyContent: "center",
+                backgroundColor: "#fff",
+              }}
+              locations={[0.5, 0.5]}
+            >
+              <Entypo name="chevron-down" size={44} color="#f7cc7f" />
+            </LinearGradient>
+          </Pressable>
+        </View>
       </BaseContainer>
-      <ActivityList activities={Mockup} />
+      <ActivityIntroduction />
+      <ActivityList setScrollPosition={setScrollPosition} activities={Mockup} />
     </ScrollContainer>
   );
 };
