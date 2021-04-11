938bbc72d43494193fc5a292d7178b030e68aaaa
André Luiz Dias de Oliveira <51346021+andr30z@users.noreply.github.com>
2021-04-11 20:46:14 -0300

    Create InitialStack.Navigation.tsx

---
+++ src/Routes/InitialStack/InitialStack.Navigation.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { CONSTANTS } from "../../Constants";
+import MainBottomNavigation from "../MainBottom/MainBottom.Navigation";
+import { WithStatusBar } from "../../Components";
+import { InitialScreen } from "../../Pages";
+import { ROUTES_NAME } from "./RoutesName";
+
+const StackTab = createStackNavigator();
+
+const InitialWithStatusBar = WithStatusBar(InitialScreen, false);
+
+/**
+ * This is the inital stack navigation container of the application.
+ * @author andr3z0
+ **/
+const InitialStackNavigation: React.FC = () => {
+  return (
+    <NavigationContainer>
+      <StackTab.Navigator
+        initialRouteName={ROUTES_NAME.INITIAL}
+        screenOptions={{
+          headerShown: false,
+        }}
+      >
+        <StackTab.Screen
+          name={ROUTES_NAME.INITIAL}
+          component={InitialWithStatusBar}
+        />
+        <StackTab.Screen
+          name={ROUTES_NAME.APP_CONTENT}
+          component={MainBottomNavigation}
+        />
+      </StackTab.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default InitialStackNavigation;
