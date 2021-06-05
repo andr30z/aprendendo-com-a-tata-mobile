60c51915057aab7b5b2f8b677da9b20e5306b8e9
André Luiz Dias de Oliveira <51346021+andr30z@users.noreply.github.com>
2021-06-05 20:32:44 -0300

    Update WithStatusBar.Component.tsx

--- src/Components/WithStatusBar/WithStatusBar.Component.tsx
+++ src/Components/WithStatusBar/WithStatusBar.Component.tsx
@@ -12,7 +12,7 @@ const StatusView: React.FC = () => (
 /**
  * High-order component that involves a certain page and gives it a margin to be spaced from the status bar, respecting the current OS
  * @param Component a React Component thats going to be wrapped
- * @param hasChildren a boolean that says if the ```Component``` wrapped makes use of children prop, this can be useful if you have a component that needs to wrap
+ * @param hasChildren a boolean that says if the ```Component``` wrapped makes use of children prop, this can be useful if you have a component that needs to have
  * the status bar margin into itself for layout purposes
  * @returns a React functional component with the space needed from the status bar;
  * @author andr3z0
