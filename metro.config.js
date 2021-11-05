163cd575bf8a9b7085c2adf6737ba22e6c587322
André Luiz Dias de Oliveira <51346021+andr30z@users.noreply.github.com>
2021-11-05 09:59:27 -0300

    Update metro.config.js

--- metro.config.js
+++ metro.config.js
@@ -2,6 +2,8 @@ const { getDefaultConfig } = require("@expo/metro-config");
 
 const defaultConfig = getDefaultConfig(__dirname);
 
+
+
 module.exports = {
   transformer: {
     babelTransformerPath: require.resolve('react-native-svg-transformer')
